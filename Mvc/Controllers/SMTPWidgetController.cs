f87b1568383972cdabacdb06ce930df0cd0c79e4
Kyle Burmark <kburmark@theprovatogroup.com>
2018-10-24 12:52:05 -0400

    Initial Checkin

---
+++ Mvc/Controllers/SMTPWidgetController.cs
@@ -0,0 +1,60 @@
+﻿
+using SitefinityWebApp.Mvc.Models;
+using System.Web.Mvc;
+using Telerik.Sitefinity.Configuration;
+using Telerik.Sitefinity.Mvc;
+using Telerik.Sitefinity.Services;
+
+namespace SitefinityWebApp.Mvc.Controllers
+{
+    [ControllerToolboxItem(Name = "SMTPWidget", Title = "SMTP Widget", SectionName = "Dashboard")]
+    public class SMTPWidgetController : Controller
+    {
+        /// <summary> 
+        /// This is the default Action. 
+        /// </summary> 
+        public ActionResult Index()
+        {
+            SystemConfig config = Config.Get<SystemConfig>();
+
+            SmtpElement smtpSettings = config.SmtpSettings;
+
+            var model = new SMTPWidgetModel
+            {
+                DefaultSenderEmailAddress = smtpSettings.DefaultSenderEmailAddress,
+                Domain = smtpSettings.Domain,
+                EnableSSL = smtpSettings.EnableSSL,
+                Host = smtpSettings.Host,
+                Password = smtpSettings.Password,
+                Port = smtpSettings.Port,
+                UserName = smtpSettings.UserName
+            };
+
+            return View(model);
+        }
+
+        //[RelativeRoute("SaveSMTPSettings")]
+        [Route("SaveSMTPSettings")]
+        [HttpPost]
+        public ActionResult SaveSMTPSettings(SMTPWidgetModel model)
+        {
+            var manager = ConfigManager.GetManager();
+
+            SystemConfig systemConfig = manager.GetSection<SystemConfig>();
+
+            SmtpElement smtpSettings = systemConfig.SmtpSettings;
+
+            smtpSettings.DefaultSenderEmailAddress = model.DefaultSenderEmailAddress;
+            smtpSettings.Domain = model.Domain;
+            smtpSettings.EnableSSL = model.EnableSSL;
+            smtpSettings.Host = model.Host;
+            smtpSettings.Password = model.Password;
+            smtpSettings.Port = model.Port;
+            smtpSettings.UserName = model.UserName;
+
+            manager.SaveSection(systemConfig);
+
+            return Json(true);
+        }
+    }
+}
---
+++ Mvc/Models/SMTPWidgetModel.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SitefinityWebApp.Mvc.Models
+{
+    public class SMTPWidgetModel
+    {
+        [Display(Name = "Default Sender Email Address")]
+        public string DefaultSenderEmailAddress { get; set; }
+
+        public string Domain { get; set; }
+
+        [Display(Name = "Enable SSL")]
+        public bool EnableSSL { get; set; }
+
+        public string Host { get; set; }
+
+        [Display(Name = "User Name")]
+        public string UserName { get; set; }
+
+        public string Password { get; set; }
+
+        public int Port { get; set; }
+    }
+}
